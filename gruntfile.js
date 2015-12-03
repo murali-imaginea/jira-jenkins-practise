34315a84d41b6e89b242a4e67404eceda2e628cc
murali-imaginea <muralikrishna.tottimpudi@imaginea.com>
2015-12-03 21:01:49 +0530

    updated grunt task v0.3
    fdfgfdgdfgdfg

--- gruntfile.js
+++ gruntfile.js
@@ -1,7 +1,15 @@
+var JenkinsUrl = "http://localhost:8080";
+var jenkinsapi = require('jenkins-api');
+var jenkins = jenkinsapi.init(JenkinsUrl);
 module.exports = function (grunt) {
   grunt.registerTask('default', function(){
+    var done = this.async();
     var buildNumber = process.env.BUILD_NUMBER;
       var buildName = process.env.JOB_NAME;
     console.log('this is grunt task boss',buildNumber , buildName );
+    jenkins.build_info(buildName, buildNumber, function(err, data) {
+        console.log('jenking changes ', data);
+        done();
+    });
   });
 };
