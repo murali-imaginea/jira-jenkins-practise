83d131bf91fbb78b4f06a23ca286c0963a0503c4
murali-imaginea <muralikrishna.tottimpudi@imaginea.com>
2015-12-03 20:57:27 +0530

    updated grunt task v0.2
    dsfsdff

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,7 @@
 module.exports = function (grunt) {
   grunt.registerTask('default', function(){
-    console.log('this is grunt task boss');
+    var buildNumber = process.env.BUILD_NUMBER;
+      var buildName = process.env.JOB_NAME;
+    console.log('this is grunt task boss',buildNumber , buildName );
   });
 };
